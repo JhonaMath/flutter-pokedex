b4e3c0196225695360b8db0aec07c67e23f2008b
Jhonathan <jhonathan.barreiro@gmail.com>
2021-06-02 14:11:55 -0300

    Publish to github-pages

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Flutter\myapp"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter\pokedex"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
